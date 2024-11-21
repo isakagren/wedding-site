a48f4622caf7ace6be52a8f6847dec6619f86ba4
Isak Ågren <isak.agren@hotmail.com>
2024-11-21 21:17:08 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
+  build: {
+    outDir: './docs',
+    emptyOutDir: true, // also necessary
+  },
   plugins: [
     vue(),
     vueJsx(),
