c0f026446bab396276ecd9cde22cc1af492ca5fb
Ågren Isak <isak.agren@bolagsverket.se>
2025-03-06 22:14:23 +0100

    utkast 1

---
+++ src/views/SwedishView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <main class="font-[Book]">
+    <div class="">
+      <nav class="flex flex-row container mx-auto p-4">
+        <ul class="grow flex flex-row">
+          <li class="mr-3"></li>
+        </ul>
+        <RouterLink to="/en"><img src="@/assets/en-flag.svg" alt="English flag"></RouterLink>
+      </nav>
+    </div>
+
+    <div class="h-lvh">
+      <img src="@/assets/images/left-forest.png" class="absolute bottom-0 left-0 max-w-7/12 -z-20">
+      <img src="@/assets/images/right-forest.png" class="absolute bottom-0 right-0 max-w-7/12 -z-10">
+
+      <div class="container mx-auto p-4 text-center">
+        <h1 class="text-ri-dark-green font-[Book] text-2xl">Välkommen att fira vårat bröllop tillsammans med oss.</h1>
+        <img src="@/assets/logos/Ri Logo - dark green.svg" class="mx-auto mt-4 max-w-9/12"/>
+        <a class="bg-ri-light-green text-ri-dark-green px-6 py-4 rounded-full font-[Heavy] mt-4 inline-block">Svara på inbjudan</a>
+      </div>
+    </div>
+
+    <div class="max-w-xl mx-auto p-4">
+      <h1 class="font-[Heavy] text-2xl text-ri-dark-green my-2">Välkommen</h1>
+      <p>Varmt välkommen att fira vårat bröllop! Vi är så glada att du vill vara med och dela denna speciella dag med oss. Mer information kommer fyllas på här så småningom. Finns det några funderingar så får ni gärna kontakta oss.</p>
+
+      <h1 class="font-[Heavy] text-2xl text-ri-dark-green mb-2 mt-8">Vigseln</h1>
+      <p>Vigseln kommer vara i Elimkyrkan klockan 12.</p>
+      <p>Entré från Fredsgatan 1, Sundsvall.</p>
+
+
+      <h1 class="font-[Heavy] text-2xl text-ri-dark-green mb-2 mt-8">Festen</h1>
+      <p>Senare fortsätter vi med en fest i Hagakyrkan klockan 16. Middag kommer serveras senare under kvällen.</p>
+      <p>Hagavägen 21, Sundsvall.</p>
+
+    </div>
+
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./src/**/*.{vue,js,ts}'],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        'ri-dark-green': '#2d4637',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
