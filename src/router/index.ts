e439dbd6cb614bdf065ff0d68dbd4668453afcf5
Ågren Isak <isak.agren@bolagsverket.se>
2025-03-06 22:42:36 +0100

    bytte till hash navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import SwedishView from '../views/SwedishView.vue'
 import EnglishView from '../views/EnglishView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -13,9 +13,6 @@ const router = createRouter({
     {
       path: '/en',
       name: 'Welcome',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: EnglishView,
     },
   ],
