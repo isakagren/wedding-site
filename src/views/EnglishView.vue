e02ae2a160e21f8c6b7383b782a7617da582bff5
Ågren Isak <isak.agren@bolagsverket.se>
2025-03-07 20:15:13 +0100

    text

--- src/views/EnglishView.vue
+++ src/views/EnglishView.vue
@@ -4,18 +4,19 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <main class="font-[Book]">
-    <div class="">
-      <nav class="flex flex-row container mx-auto p-4">
-        <ul class="grow flex flex-row">
-          <li class="mr-3"></li>
-        </ul>
-        <RouterLink to="/"><img src="@/assets/se-flag.svg" alt="Swedish flag"></RouterLink>
-      </nav>
-    </div>
 
     <div class="h-lvh">
       <img src="@/assets/images/left-forest.png" class="absolute bottom-0 left-0 lg:max-w-7/12 -z-20 max-w-10/12 sm:max-w-9/12">
       <img src="@/assets/images/right-forest.png" class="absolute bottom-0 right-0 lg:max-w-7/12 -z-10 max-w-10/12 sm:max-w-9/12">
+      
+      <div class="">
+        <nav class="flex flex-row container mx-auto p-4">
+          <ul class="grow flex flex-row">
+            <li class="mr-3"></li>
+          </ul>
+          <RouterLink to="/"><img src="@/assets/se-flag.svg" alt="Swedish flag"></RouterLink>
+        </nav>
+      </div>
 
       <div class="container mx-auto p-4 text-center">
         <h1 class="text-ri-dark-green font-[Book] text-2xl">Welcome to celebrate our wedding together with us.</h1>
@@ -24,7 +25,7 @@ import { RouterLink } from 'vue-router';
       </div>
     </div>
 
-    <div class="max-w-xl mx-auto p-4" style="margin-top: -4em">
+    <div class="max-w-xl mx-auto p-4">
       <h1 class="font-[Heavy] text-2xl text-ri-green my-2">Welcome</h1>
       <p>Warm welcome to our wedding! We are so happy that you want to join us and share this special day with us. If you have any questions, please feel free to contact us.</p>
       <br>
--- src/views/SwedishView.vue
+++ src/views/SwedishView.vue
@@ -4,18 +4,18 @@ import { RouterLink } from 'vue-router';
 
 <template>
   <main class="font-[Book]">
-    <div class="">
-      <nav class="flex flex-row container mx-auto p-4">
-        <ul class="grow flex flex-row">
-          <li class="mr-3"></li>
-        </ul>
-        <RouterLink to="/en"><img src="@/assets/en-flag.svg" alt="English flag"></RouterLink>
-      </nav>
-    </div>
-
     <div class="h-lvh">
       <img src="@/assets/images/left-forest.png" class="absolute bottom-0 left-0 lg:max-w-7/12 -z-20 max-w-10/12 sm:max-w-9/12">
       <img src="@/assets/images/right-forest.png" class="absolute bottom-0 right-0 lg:max-w-7/12 -z-10 max-w-10/12 sm:max-w-9/12">
+
+      <div class="">
+        <nav class="flex flex-row container mx-auto p-4">
+          <ul class="grow flex flex-row">
+            <li class="mr-3"></li>
+          </ul>
+          <RouterLink to="/en"><img src="@/assets/en-flag.svg" alt="English flag"></RouterLink>
+        </nav>
+      </div>
 
       <div class="container mx-auto p-4 text-center">
         <h1 class="text-ri-dark-green font-[Book] text-2xl">Välkommen att fira vårt bröllop tillsammans med oss.</h1>
@@ -24,7 +24,7 @@ import { RouterLink } from 'vue-router';
       </div>
     </div>
 
-    <div class="max-w-xl mx-auto p-4" style="margin-top: -4em">
+    <div class="max-w-xl mx-auto p-4">
       <h1 class="font-[Heavy] text-2xl text-ri-green my-2">Välkommen</h1>
       <p>Varmt välkommen att fira vårat bröllop! Vi är så glada att du vill vara med och dela denna speciella dag med oss. Finns det några funderingar så får ni gärna kontakta oss.</p>
       <br>
